93befd1da812395ceabcf3b976eced35bc34bdc8
AstraEL-15 <ernestlenard1234@gmail.com>
2026-09-19 20:35:18 +0800

    Update db.php and initialize.php with working Aiven connection

--- lab_07_create_user/db.php
+++ lab_07_create_user/db.php
@@ -1,13 +1,15 @@
 <?php
-$host = trim(getenv('DB_HOST') ?: 'localhost');
-$user = trim(getenv('DB_USER') ?: 'root');
+$host = trim(getenv('DB_HOST') ?: 'mysql-22f856ab-ernestlenard1234-479d.c.aivencloud.com');
+$user = trim(getenv('DB_USER') ?: 'avnadmin');
 $pass = trim(getenv('DB_PASS') ?: '');
 $db   = trim(getenv('DB_NAME') ?: 'defaultdb');
-$port = trim(getenv('DB_PORT') ?: 3306);
+$port = trim(getenv('DB_PORT') ?: 28562);
 
 $conn = mysqli_init();
 $ca_path = file_exists(__DIR__ . '/ca.pem') ? __DIR__ . '/ca.pem' : __DIR__ . '/../ca.pem';
 $conn->ssl_set(NULL, NULL, $ca_path, NULL, NULL);
+
+// Keep MYSQLI_CLIENT_SSL as the last argument
 $conn->real_connect($host, $user, $pass, $db, (int)$port, NULL, MYSQLI_CLIENT_SSL);
 
 if ($conn->connect_error) {
