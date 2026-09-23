5f4f5467c930a96b479b9bf52d404198e645a1a9
AstraEL-15 <ernestlenard1234@gmail.com>
2026-09-23 15:02:57 +0800

    Initialize retesting

--- api/lab_10_delete_user/initialize.php
+++ api/lab_10_delete_user/initialize.php
@@ -1,5 +1,9 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params(['path' => '/']);
+    session_start();
+}
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
--- api/lab_11_login_sessions/initialize.php
+++ api/lab_11_login_sessions/initialize.php
@@ -1,5 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
+    // Force session cookie to be accessible across all subfolders
+    session_set_cookie_params(['path' => '/']);
     session_start();
 }
 
